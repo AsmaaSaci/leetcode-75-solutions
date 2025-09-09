21d09d194d2ef4ac3758883bfc7dc08ad7956c59
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-09 19:39:52 +0000

    Add solution for 334. Increasing Triplet Subsequence

---
+++ 334. Increasing Triplet Subsequence.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+bool increasingTriplet(vector<int>& nums) {
+    int first = INT_MAX, second = INT_MAX;
+    for (int num : nums) {
+        if (num <= first) {
+            first = num;  
+        } else if (num <= second) {
+            second = num; 
+        } else {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    vector<int> nums1 = {1, 2, 3, 4, 5};
+    vector<int> nums2 = {5, 4, 3, 2, 1};
+    vector<int> nums3 = {2, 1, 5, 0, 4, 6};
+
+    cout << (increasingTriplet(nums1) ? "true" : "false") << endl; // true
+    cout << (increasingTriplet(nums2) ? "true" : "false") << endl; // false
+    cout << (increasingTriplet(nums3) ? "true" : "false") << endl; // true
+
+    return 0;
+}
