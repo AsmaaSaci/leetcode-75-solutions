41fe0f8b3c468025a036b18fa73454bed6b43291
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-11 14:04:06 +0000

    add solution: equal row and column pairs

---
+++ 2352-equal-row-and-column-pairs/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        int n = grid.size();
+        map<vector<int>, int> rowCount;
+
+        // عد الصفوف
+        for (int i = 0; i < n; i++) {
+            rowCount[grid[i]]++;
+        }
+
+        int result = 0;
+        // شكل الأعمدة
+        for (int j = 0; j < n; j++) {
+            vector<int> col;
+            for (int i = 0; i < n; i++) {
+                col.push_back(grid[i][j]);
+            }
+            if (rowCount.count(col)) {
+                result += rowCount[col];
+            }
+        }
+
+        return result;
+    }
+};
