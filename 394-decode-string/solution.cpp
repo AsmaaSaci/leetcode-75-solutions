073445c6b5f229c36cafdebf965d31975a0e0140
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-12 10:44:42 +0000

    add solution: decode string

---
+++ 394-decode-string/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string decodeString(string s) {
+    stack<int> counts;
+    stack<string> resultStack;
+    string result;
+    int num = 0;
+    for (char c : s) {
+        if (isdigit(c)) {
+            num = num * 10 + (c - '0');
+        } else if (c == '[') {
+            counts.push(num);
+            resultStack.push(result);
+            num = 0;
+            result = "";
+        } else if (c == ']') {
+            int k = counts.top(); counts.pop();
+            string prev = resultStack.top(); resultStack.pop();
+            string temp;
+            for (int i = 0; i < k; i++) temp += result;
+            result = prev + temp;
+        } else {
+            result += c;
+        }
+    }
+    return result;
+}
+
+int main() {
+    string s = "3[a2[c]]";
+    cout << decodeString(s) << endl;
+    return 0;
+}
