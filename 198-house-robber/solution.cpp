018682247c81dd91571eb04c7fa384f9916dd782
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-19 09:27:32 +0000

    add solution: house robber

---
+++ 198-house-robber/solution.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int rob(vector<int>& nums) {
+    int n = nums.size();
+    if (n == 0) return 0;
+    if (n == 1) return nums[0];
+    int prev2 = nums[0];
+    int prev1 = max(nums[0], nums[1]);
+    for (int i = 2; i < n; i++) {
+        int cur = max(prev1, prev2 + nums[i]);
+        prev2 = prev1;
+        prev1 = cur;
+    }
+    return prev1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
+    cout << rob(nums) << endl;
+    return 0;
+}
